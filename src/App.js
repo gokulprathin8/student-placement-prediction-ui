1931199144c26fbeb1fc6308219ed25eeac709d7
Gokul Prathin <gokulprathin8@gmail.com>
2020-11-26 20:34:20 +0530

    Changes to Homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import HeaderFooter from "./Components/Common";
+import Slider from "react-slick";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Row from "react-bootstrap/cjs/Row";
+import Col from "react-bootstrap/cjs/Col";
+
 
 function App() {
+    var settings = {
+        dots : true
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <HeaderFooter>
+          <div style={{ padding: "40px", background: "lightgray", margin: "auto" }}>
+              <Slider {...settings}>
+                  <div>
+                      <img src="https://webmaileventscms.gitam.edu/assets/uploads/eventsforwebsites/2aa500954d1ed256c39ff72c6d34439e_eventsforwebsites.jpg"  width="100%" height="250px"/>
+                  </div>
+                  <div>
+                      <img src="https://www.gitam.edu/campus_cms/assets/uploads/slider/1602241365_slider.jpg" width="100%" height="250px"/>
+                  </div>
+                  <div>
+                      <img src="https://www.gitam.edu/campus_cms/assets/uploads/slider/1605935704_slider.jpg" width="100%" height="250px"/>
+                  </div>
+                  <div>
+                      <img src="https://www.gitam.edu/campus_cms/assets/uploads/slider/1581662296_slider.jpg" width="100%" height="250px"/>
+                  </div>
+              </Slider>
+          </div>
+
+          <Row>
+              <Col>
+                  <div style={{ border: "1px solid black" }}>
+                    <p>wrkgjb</p>
+                  </div>
+              </Col>
+              <Col>
+                  <div style={{ border: "1px solid black" }}>
+                      <p>wrkgjb</p>
+                  </div>
+              </Col>
+              <Col>
+                  <div style={{ border: "1px solid black" }}>
+                      <p>wrkgjb</p>
+                  </div>
+              </Col>
+          </Row>
+      </HeaderFooter>
   );
 }
 
